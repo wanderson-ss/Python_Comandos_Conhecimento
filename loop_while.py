40c22d350b687099bc92e64fc32b506748b5902a
Wanderson Santos Sousa <107068237+wanderson-ss@users.noreply.github.com>
2023-12-11 10:51:14 -0300

    Create loop_while.py

---
+++ loop_while.py
@@ -0,0 +1,28 @@
+"""
+Loop while
+Forma geral:
+While expressão booleana
+    //execução do loop
+
+O bloco do while será repetido , enqaunto a condição boleana for verdadeira
+
+
+Expressão booleanda é o resultado F e V
+
+numero = 1
+
+while numero < 10:
+    print(numero)
+    numero = numero + 1
+
+# LOOP infinito, sempre se atentar no fator declarado
+"""
+
+resposta = ''
+
+while resposta != 'sim':
+    resposta = input('Teste ?')
+
+
+
+#Lembresse de fazer o codigo parar
